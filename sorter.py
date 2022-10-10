c24fc21320100a6af027d68186393cf2fd53dfae
khashashin <khashashin@gmail.com>
2022-10-10 03:47:07 +0200

    update

--- sorter.py
+++ sorter.py
@@ -2,7 +2,7 @@ import json
 
 
 def sort_dict():
-    query = 'гар'
+    query = 'сто'
     # open new_data.json file as python dictionary
     with open('new_data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
